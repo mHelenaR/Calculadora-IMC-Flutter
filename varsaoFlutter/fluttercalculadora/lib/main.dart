454278c06662cf2a4041c0b74314836ddc49853f
maria <mariahelenarocha54@gmail.com>
2022-07-21 15:45:39 -0300

    ultimo commit

--- varsaoFlutter/fluttercalculadora/lib/main.dart
+++ varsaoFlutter/fluttercalculadora/lib/main.dart
@@ -3,10 +3,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
 }
 
 class Home extends StatefulWidget {
